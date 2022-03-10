558fada137c5a1c969976723affc700d0b589056
Yi Xiang Lim <yixiang_lim@intuit.com>
2022-03-10 02:13:35 +0000

    Updated code to include dropdown

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-
 import plotly.graph_objs as go
 import pandas as pd
+from dash.dependencies import Input, Output, State
 
 ########### Define your variables ######
 
-tabtitle = 'Old McDonald'
-sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/agriculture-exports-map'
 # here's the list of possible columns to choose from.
-list_of_columns =['total exports', 'beef', 'pork', 'poultry',
+list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 'poultry',
        'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
+list_of_products = ['beef', 'pork', 'poultry',
+       'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
+       'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton'] 
+
+mycolumn='corn'
+myheading1 = f"Wow! That's a lot of {mycolumn}!"
+mygraphtitle = '2011 US Agriculture Exports by State'
+mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+mycolorbartitle = "Millions USD"
+tabtitle = 'Agriculture Data'
+sourceurl = 'https://plot.ly/python/choropleth-maps/'
+githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 
 
 ########## Set up the chart
@@ -31,50 +39,47 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1('2011 Agricultural Exports, by State'),
-    html.Div([
-        html.Div([
-                html.H6('Select a variable for analysis:'),
-                dcc.Dropdown(
-                    id='options-drop',
-                    options=[{'label': i, 'value': i} for i in list_of_columns],
-                    value='corn'
-                ),
-        ], className='two columns'),
-        html.Div([dcc.Graph(id='figure-1'),
-            ], className='ten columns'),
-    ], className='twelve columns'),
+    html.H1(myheading1),
+    dcc.Dropdown(
+        id='your_input_here',
+        options=[
+                {'label':list_of_products[0], 'value':list_of_products[0]},
+                {'label':list_of_products[1], 'value':list_of_products[1]},
+                {'label':list_of_products[2], 'value':list_of_products[2]},
+                {'label':list_of_products[3], 'value':list_of_products[3]},
+                ],
+        value='corn',
+        ),
+    dcc.Graph(
+        id='figure-1',
+    ),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
 
-
-# make a function that can intake any varname and produce a map.
+########## Define Callback
 @app.callback(Output('figure-1', 'figure'),
-             [Input('options-drop', 'value')])
-def make_figure(varname):
-    mygraphtitle = f'Exports of {varname} in 2011'
-    mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-    mycolorbartitle = "Millions USD"
+              [Input('your_input_here', 'value')])
+def makemap(mycolumn):
 
-    data=go.Choropleth(
+    fig = go.Figure(data=go.Choropleth(
         locations=df['code'], # Spatial coordinates
+        z = df[mycolumn].astype(float), # Data to be color-coded
         locationmode = 'USA-states', # set of locations match entries in `locations`
-        z = df[varname].astype(float), # Data to be color-coded
         colorscale = mycolorscale,
         colorbar_title = mycolorbartitle,
-    )
-    fig = go.Figure(data)
+    ))
+
     fig.update_layout(
         title_text = mygraphtitle,
         geo_scope='usa',
         width=1200,
         height=800
-    )
-    return fig
-
+    )   
+    
+    return fig 
 
 ############ Deploy
 if __name__ == '__main__':
